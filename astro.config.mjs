e3c44c04b87eb44d61fe7e63f9e3fb412bdda87c
Makusu-dev <maxime.jeannin@hotmail.fr>
2025-10-27 18:25:11 +0100

    supression de ka config astro car redondant avec le workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://makusu-dev.github.io/",
-    base: '/astro-page',
 });
