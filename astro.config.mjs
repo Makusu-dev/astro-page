615f40922883a7a23a9643d6f6a764a6ec60e083
Makusu-dev <maxime.jeannin@hotmail.fr>
2025-10-27 18:28:20 +0100

    retour a la config déns astro, simplification du workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+      site: 'https://makusu-dev.github.io',
+      base: '/astro-page'
 });
