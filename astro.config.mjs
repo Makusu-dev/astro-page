ba9f086ce2ffe612c3f2029df155271c69376da3
Makusu-dev <maxime.jeannin@hotmail.fr>
2025-10-27 18:14:05 +0100

    construction d'url avec new URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://makusu-dev.github.io/astro-page/",
+    site: "https://makusu-dev.github.io/",
     base: '/astro-page',
 });
