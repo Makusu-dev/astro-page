b61d3ddd34ed5c01c9c09024aa2ef1ca76579150
Makusu-dev <maxime.jeannin@hotmail.fr>
2025-10-27 18:55:07 +0100

    correction du utils qui retirait le slash du base_url

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,5 @@
 export function url(path: string): string {
-  const base = import.meta.env.BASE_URL;
-  // Assure qu'il n'y a pas de double slash
-  const cleanPath = path.startsWith('/') ? path.slice(1) : path;
-  return `${base}${cleanPath}`;
+  const base = import.meta.env.BASE_URL.replace(/\/$/, ''); // Enlève le slash final de base
+  const cleanPath = path.startsWith('/') ? path : `/${path}`; // Assure que path commence par /
+  return base + cleanPath;
 }
